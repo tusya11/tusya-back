1dbc7e2c0b42118b2eb6b9cfa287b7755c6c0acc
Georgiy Malahov <stepllerrus78@gmail.com>
2023-12-15 23:10:01 +0300

    asd

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -4,16 +4,15 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
-class CartResource extends JsonResource
-{
+class CartResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
-    {
+    public function toArray(Request $request): array {
         return [
             "id" => $this->id,
             "category_id" => $this->category_id,
@@ -21,8 +20,32 @@ class CartResource extends JsonResource
             "name" => $this->name,
             "price" => $this->price,
             "description" => $this->description,
+            'rating' => self::calculateRating($this->ratings),
             "image" => $this->image ? config('app.url') . $this->image : null,
-            'is_favorite' => auth()->user()->favorites->where('product_id', $this->id)->count() ? true : false,
+            'is_favorite' => self::checkOnIsFavorite($this->id),
         ];
+    }
+
+    private static function checkOnIsFavorite($productId) {
+        if (Auth::guard('api')->check()) {
+            if (Auth::guard('api')->user()->favorites->where('product_id', $productId)->count()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private static function calculateRating($ratings) {
+        $resultRating = 0;
+        $amount = $ratings->count();
+
+        foreach ($ratings as $ratingProduct) {
+            $resultRating += $ratingProduct->rating;
+        }
+
+        return $amount ? round(($resultRating / $amount), 1) : 0;
     }
 }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -6,15 +6,13 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
 
-class ProductResource extends JsonResource
-{
+class ProductResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
-    {
+    public function toArray(Request $request): array {
         return [
             "id" => $this->id,
             "category_id" => $this->category_id,
@@ -22,13 +20,13 @@ class ProductResource extends JsonResource
             "name" => $this->name,
             "price" => $this->price,
             "description" => $this->description,
+            'rating' => self::calculateRating($this->ratings),
             "image" => $this->image ? config('app.url') . $this->image : null,
             'is_favorite' => self::checkOnIsFavorite($this->id),
         ];
     }
 
-    private static function checkOnIsFavorite($productId)
-    {
+    private static function checkOnIsFavorite($productId) {
         if (Auth::guard('api')->check()) {
             if (Auth::guard('api')->user()->favorites->where('product_id', $productId)->count()) {
                 return true;
@@ -38,5 +36,16 @@ class ProductResource extends JsonResource
         } else {
             return false;
         }
+    }
+
+    private static function calculateRating($ratings) {
+        $resultRating = 0;
+        $amount = $ratings->count();
+
+        foreach ($ratings as $ratingProduct) {
+            $resultRating += $ratingProduct->rating;
+        }
+
+        return $amount ? round(($resultRating / $amount), 1) : 0;
     }
 }
